8468c3a431fa18c243d9ef7020b16783444eab68
Rafa <rafaeldelriofranco@gmail.com>
2016-11-11 15:39:16 +0100

    Inicio de proyecto ejemplo con maven, introducido un caso de prueba con la libreria jgrapht

---
+++ src/test/java/ejemplo/AppTest.java
@@ -0,0 +1,42 @@
+package ejemplo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+ 	UndirectedGraph<String, DefaultEdge> stringGraph = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+    }
+
+}
